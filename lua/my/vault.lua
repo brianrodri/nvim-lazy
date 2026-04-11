f795e6ba11397f6db75cde316dc6594b6e5c69e9
Brian Rodriguez <brian@brianrodri.com>
2026-04-10 23:30:06 -0400

    Assign strict types to all outputs

--- lua/my/vault.lua
+++ lua/my/vault.lua
@@ -1,25 +1,31 @@
 local M = {}
 local H = {}
 
+---@param opts obsidian.config|{}
 function M.obsidian_config(_, opts)
   require("obsidian").setup(opts)
   vim.api.nvim_create_autocmd("User", { pattern = "ObsidianNoteEnter", callback = H.on_obsidian_note_entered })
 end
 
+---@return boolean? enabled
 function M.obsidian_is_enabled()
   -- TODO
   return true
 end
 
+---@type obsidian.config|{}
 M.obsidian_opts = {
   -- TODO
 }
 
+---@type LazyKeys[]
 M.obsidian_keys = {
   -- TODO
 }
 
-function H.on_obsidian_note_entered()
+---@param args vim.api.keyset.create_autocmd.callback_args
+---@return boolean? delete_after
+function H.on_obsidian_note_entered(args)
   -- TODO
 end
 
