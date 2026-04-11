ef3dec5766e40c928d1385951a3e12c518e6d365
Brian Rodriguez <brian@brianrodri.com>
2026-04-10 22:49:42 -0400

    Personalize default Neovim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- UI and Layout
+vim.o.ruler = true
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.colorcolumn = "+1"
+vim.opt.scrolloff = 0
