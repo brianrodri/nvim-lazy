acd95e0a7a75551922003bd83ab65c1044c206b9
Brian Rodriguez <brian@brianrodri.com>
2026-04-12 07:33:18 -0400

    setup dashboard

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,23 @@ return {
     "everviolet/nvim",
     name = "evergarden",
     priority = 1000,
+    ---@type evergarden.types.config|{}
     opts = {
       theme = { variant = "winter" },
-      editor = { transparent_background = true },
+      overrides = function(colors)
+        return {
+          SnacksDashboardHeader = { fg = colors.green },
+          SnacksDashboardIcon = { fg = colors.green },
+          SnacksDashboardDesc = { fg = colors.text },
+          SnacksDashboardKey = { fg = colors.green },
+          SnacksDashboardFooter = { fg = colors.comment },
+          SnacksDashboardSpecial = { fg = colors.green },
+          SnacksDashboardDir = { fg = colors.comment },
+          SnacksDashboardFile = { fg = colors.text },
+          SnacksDashboardTerminal = { fg = colors.text },
+          SnacksDashboardTitle = { fg = colors.green, style = { "bold" } },
+        }
+      end,
     },
     config = function(_, opts)
       require("evergarden").setup(opts)
