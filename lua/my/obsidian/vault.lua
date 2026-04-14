b8f9c0504ef8d778dae5e35a892f41f66349ef36
Brian Rodriguez <brian@brianrodri.com>
2026-04-14 19:26:35 -0400

    Setup Dashboard & Obsidian (#3)
    * Personalize default Neovim options
    
    * Add skeleton for my vault config
    
    * Assign strict types to all outputs
    
    * Refactor into smaller objects
    
    * Move key specs into plugin spec
    
    * Fix typo in export name
    
    * Minor cleanup
    
    * Refactor into stateful class
    
    * Add logic for creating broad/narrow notes
    
    * Lazy Update
    
    * Disable auto-updates
    
    * Refactor external API into black box of functions
    
    * Refactor: gather into related layers
    
    * Undo unintended changes
    
    * Standardize scope names
    
    * Address comments
    
    * Address comments
    
    * setup dashboard
    
    * fix daily note keymap
    
    * Add defensive checks when manipulating tagstack
    
    * Refactor to make code simpler
    
    * Always keep the normalized root
    
    * Iterate over expectations rather than actual
    
    * Defensive check against invalid line numbers
    
    * Use transparent background
    
    * Add my utils
    
    * Delete picker from Vault interface
    
    * Split into vault/bookmark classes
    
    * Address comments
    
    * Fix buffer spec
    
    * Changes to consistency
    
    * Refactor into small helpers
    
    * Fix varargs truncation
    
    * Protect against unshown source notes

---
+++ lua/my/obsidian/vault.lua
@@ -0,0 +1,106 @@
+local note_ext = require("my.obsidian.note_ext")
+
+---@class my.obsidian.Vault
+---@field name string
+---@field root string
+---@field fleeting_notes_folder string
+---@field daily_notes_folder string
+---@field attachments_folder string
+---@field templates_folder string
+local Vault = {}
+
+function Vault:exists()
+  local stat = vim.uv.fs_stat(self.root)
+  return stat and stat.type == "directory"
+end
+
+function Vault:pick_recent() require("snacks.picker").recent({ filter = { cwd = self.root } }) end
+
+function Vault:get_workspace_spec()
+  ---@type obsidian.workspace.WorkspaceSpec
+  return {
+    name = self.name,
+    path = self.root,
+    ---@diagnostic disable-next-line: missing-fields
+    overrides = {
+      daily_notes = { folder = self.daily_notes_folder, workdays_only = false, default_tags = {} },
+      attachments = { folder = self.attachments_folder },
+      frontmatter = { enabled = function(rel_path) return vim.fs.dirname(rel_path) == self.fleeting_notes_folder end },
+      ---@diagnostic disable-next-line: missing-fields
+      templates = { folder = self.templates_folder },
+      notes_subdir = self.fleeting_notes_folder,
+      new_notes_location = "notes_subdir",
+    },
+  }
+end
+
+---@class my.obsidian.LinkedNoteOpts
+local DEFAULT_LINKED_NOTE_OPTS = {
+  ---@type string?
+  link_fmt = "- %s",
+
+  ---@type integer?
+  src_buf = nil,
+  ---@type obsidian.Note?
+  src_note = nil,
+  ---@type obsidian.note.InsertTextOpts|{}
+  src_insert_opts = {
+    placement = "bot",
+    section = { header = "Outgoing Links", level = 2, on_missing = "create" },
+  },
+
+  ---@type obsidian.Note?
+  dst_note = nil,
+  ---@type obsidian.note.InsertTextOpts|{}
+  dst_insert_opts = {
+    placement = "bot",
+    section = { header = "Incoming Links", level = 2, on_missing = "create" },
+  },
+}
+
+---@param ... {}|my.obsidian.LinkedNoteOpts
+function Vault:new_linked_note(...)
+  local obsidian_api = require("obsidian.api")
+  local obsidian_unique = require("obsidian.unique")
+
+  local opt_parts = vim.list_extend({ ... }, { DEFAULT_LINKED_NOTE_OPTS })
+  local opts = vim.tbl_deep_extend("keep", {}, table.unpack(opt_parts))
+
+  local src_note = opts.src_note or obsidian_api.current_note(opts.src_buf or 0)
+  assert(src_note, "Failed to resolve source note")
+  local dst_note = opts.dst_note or obsidian_unique.new_unique_note(nil, { should_write = true })
+  assert(dst_note, "Failed to resolve destination note")
+  assert(not note_ext.equal(src_note, dst_note), "Refused to create self-referential link")
+
+  local link_to_dst_note = opts.link_fmt:format(dst_note:format_link())
+  local src_col = link_to_dst_note:len() + 1
+  local src_line = src_note:insert_text(link_to_dst_note, opts.src_insert_opts)
+  assert(src_line > 0, "Failed to insert link into source note")
+
+  local src_win = vim.fn.bufwinid(src_note.bufnr)
+  if src_win > -1 then
+    local new_item = { tagname = dst_note.id, from = { src_note.bufnr, src_line, src_col, 0 } }
+    vim.fn.settagstack(src_win, { items = { new_item } }, "t")
+  end
+
+  dst_note:open({
+    callback = vim.schedule_wrap(function()
+      local link_to_src_note = opts.link_fmt:format(src_note:format_link())
+      local dst_col = link_to_src_note:len() + 1
+      local dst_line = dst_note:insert_text(link_to_src_note, opts.dst_insert_opts)
+      assert(dst_line > 0, "Failed to insert link into destination note")
+
+      vim.schedule(function() dst_note:open({ line = dst_line, col = dst_col }) end)
+    end),
+  })
+end
+
+local M = {}
+
+function M.new(opts)
+  local self = setmetatable(opts or {}, { __index = Vault })
+  self.root = vim.fs.normalize(self.root or "")
+  return self
+end
+
+return M
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,70 @@
+local bookmark = require("my.obsidian.bookmark")
+local my_vault = require("my.obsidian.vault")
+
+local BOOKMARK = bookmark.new()
+
+local VAULT = my_vault.new({
+  name = "My Vault",
+  root = "~/Vault",
+  fleeting_notes_folder = "2. Fleeting",
+  daily_notes_folder = "1. Journal/1. Daily",
+  attachments_folder = "9. Meta/Attachments",
+  templates_folder = "9. Meta/Templates",
+})
+
+---@type my.obsidian.LinkedNoteOpts
+local NARROW_OPTS = {
+  src_insert_opts = { section = { header = "Narrower", level = 2 } },
+  dst_insert_opts = { section = { header = "Broader", level = 2 } },
+}
+
+---@type my.obsidian.LinkedNoteOpts
+local BROADEN_OPTS = {
+  src_insert_opts = { section = { header = "Broader", level = 2 } },
+  dst_insert_opts = { section = { header = "Narrower", level = 2 } },
+}
+
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "obsidian-nvim/obsidian.nvim",
+    commit = "f816915e0bf2f60f44d23a5e3d59658fa8a20094",
+    dependencies = { "nvim-lua/plenary.nvim", "folke/which-key.nvim", "folke/snacks.nvim" },
+    opts = {
+      workspaces = { VAULT:get_workspace_spec() },
+      ui = { enable = false },
+      -- TODO: Delete after 4.0.0 release
+      legacy_commands = false,
+    },
+    config = function(_, opts)
+      require("obsidian").setup(opts)
+      vim.api.nvim_create_autocmd("User", {
+        group = vim.api.nvim_create_augroup("MyObsidianKeymaps", { clear = true }),
+        pattern = "ObsidianNoteEnter",
+        callback = function(args)
+          local link_opts = { src_buf = args.buf }
+          require("which-key").add({
+            buffer = args.buf,
+            { "<leader>vp", function() BOOKMARK:toggle_buffer(args.buf) end, desc = "Pick Bookmark" },
+            { "<leader>vj", function() VAULT:new_linked_note(link_opts, NARROW_OPTS) end, desc = "Make Narrower Note" },
+            { "<leader>vk", function() VAULT:new_linked_note(link_opts, BROADEN_OPTS) end, desc = "Make Broader Note" },
+          })
+        end,
+      })
+    end,
+    enabled = VAULT:exists(),
+    keys = {
+      { "<leader>vn", ":Obsidian new<cr>", desc = "New Note", silent = true },
+      { "<leader>vN", ":Obsidian new<cr><cr>", desc = "New Note", silent = true },
+      { "<leader>vs", ":Obsidian search<cr>", desc = "Search Notes", silent = true },
+      { "<leader>vf", ":Obsidian quick_switch<cr>", desc = "Find Note", silent = true },
+      { "<leader>vo", ":Obsidian open<cr>", desc = "Open Obsidian", silent = true },
+      { "<leader>vy", ":Obsidian extract_note<cr>", desc = "Extract to Note", mode = { "n", "v" }, silent = true },
+      { "<leader>vt", ":Obsidian today<cr>", desc = "Today's Note", silent = true },
+      { "<leader>vr", function() VAULT:pick_recent() end, desc = "Recent Notes", silent = true },
+      { "<leader>vv", function() BOOKMARK:open_or_pick() end, desc = "Open Bookmark", silent = true },
+      { "<leader>va", function() BOOKMARK:append_text() end, desc = "Append To Bookmark", silent = true },
+    },
+  },
+}
