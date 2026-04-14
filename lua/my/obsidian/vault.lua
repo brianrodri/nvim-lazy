a8344a61ce5fbd7f791916ab8506ae668604ad2d
Brian Rodriguez <brian@brianrodri.com>
2026-04-13 23:42:19 -0400

    Address comments

--- lua/my/obsidian/vault.lua
+++ lua/my/obsidian/vault.lua
@@ -60,25 +60,27 @@ function Vault:new_linked_note(...)
   local src_note = opts.src_note or obsidian_api.current_note(opts.src_buf or 0)
   local dst_note = opts.dst_note or obsidian_unique.new_unique_note(nil, { should_write = true })
   assert(src_note, "src_note must be provided")
-  assert(dst_note, "src_note must be provided")
+  assert(dst_note, "dst_note must be provided")
 
   local dst_link = opts.link_fmt:format(dst_note:format_link())
-  local src_line = src_note:insert_text(dst_link, opts.src_insert_opts)
-  assert(src_line > 0, "Failed to insert link into source note")
+  local src_lnum = src_note:insert_text(dst_link, opts.src_insert_opts)
+  assert(src_lnum > 0, "Failed to insert link into source note")
 
-  if opts.src_buf then
-    local src_col = dst_link:len() + 1
-    local item = { tagname = dst_note.id, from = { opts.src_buf, src_line, src_col, 0 } }
-    vim.fn.settagstack(vim.fn.win_getid(), { items = { item } }, "t")
+  local buf_pos = vim.api.nvim_buf_call(src_note.bufnr, function() return vim.fn.getpos(".") end)
+  if buf_pos[1] == src_note.bufnr then
+    buf_pos[2] = src_lnum
+    buf_pos[3] = dst_link:len() + 1
+    local item = { tagname = dst_note.id, from = buf_pos }
+    vim.fn.settagstack(vim.fn.bufwinid(opts.src_buf), { items = { item } }, "t")
   end
 
   dst_note:open({
     callback = vim.schedule_wrap(function()
       local src_link = opts.link_fmt:format(src_note:format_link())
-      local dst_line = dst_note:insert_text(src_link, opts.dst_insert_opts)
-      assert(dst_line > 0, "Failed to insert link into destination note")
+      local dst_lnum = dst_note:insert_text(src_link, opts.dst_insert_opts)
+      assert(dst_lnum > 0, "Failed to insert link into destination note")
       local dst_col = src_link:len() + 1
-      vim.schedule(function() dst_note:open({ line = dst_line, col = dst_col }) end)
+      vim.schedule(function() dst_note:open({ line = dst_lnum, col = dst_col }) end)
     end),
   })
 end
@@ -87,7 +89,7 @@ local M = {}
 
 function M.new(opts)
   local self = setmetatable(opts or {}, { __index = Vault })
-  self.root = vim.fs.normalize(self.root or "")
+  self.root = vim.fs.normalize(vim.fn.expand(self.root or ""))
   return self
 end
 
