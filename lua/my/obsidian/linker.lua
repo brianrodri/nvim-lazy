369502e47b560665e0641b8cd0677ddf366219bb
Brian Rodriguez <brian@brianrodri.com>
2026-04-15 09:10:51 -0400

    Address comments

--- lua/my/obsidian/linker.lua
+++ lua/my/obsidian/linker.lua
@@ -18,7 +18,7 @@ local DEFAULT_LINK_OPTS = {
   },
 }
 
----@type table<string, fun(callback: fun(note?: obsidian.Note)): obsidian.Note|?>
+---@type table<string, fun(callback: fun(note?: obsidian.Note))>
 local AUTO_RESOLVE = {}
 
 ---@param ... my.obsidian.linker.LinkOpts
@@ -26,29 +26,29 @@ function M.new(...)
   local opts = vim.tbl_deep_extend("force", vim.deepcopy(DEFAULT_LINK_OPTS), ...)
 
   H.resolve_note(opts.src_note, function(src_note)
-    assert(src_note and src_note:exists(), "Source could not be resolved")
     H.resolve_note(opts.dst_note, function(dst_note)
-      assert(dst_note and dst_note:exists(), "Destination could not be resolved")
       assert(tostring(src_note.path) ~= tostring(dst_note.path), "Source and Destination must be different")
 
       local link_to_dst_note = opts.link_fmt:format(dst_note:format_link())
-      local src_col = link_to_dst_note:len() + 1
-      local src_buf = src_note.bufnr
       local src_lnum = src_note:insert_text(link_to_dst_note, opts.src_insert_opts)
       assert(src_lnum > 0, "Source could not be changed")
 
-      if src_buf == 0 or src_buf == vim.api.nvim_win_get_buf(0) then
-        vim.fn.settagstack(0, { items = { { tagname = dst_note.id, from = { src_buf, src_lnum, src_col, 0 } } } }, "t")
+      local src_buf = src_note.bufnr or -1
+      local src_win = src_buf > 0 and vim.fn.bufwinid(src_buf) or src_buf
+      if src_buf ~= -1 and src_win ~= -1 then
+        local src_col = link_to_dst_note:len() + 1
+        local src_pos = { src_buf, src_lnum, src_col, 0 }
+        vim.fn.settagstack(src_win, { items = { { tagname = dst_note.id, from = src_pos } } }, "t")
       end
 
       dst_note:open({
         sync = true,
         callback = function()
           local link_to_src_note = opts.link_fmt:format(src_note:format_link())
-          local dst_col = link_to_src_note:len() + 1
           local dst_lnum = dst_note:insert_text(link_to_src_note, opts.dst_insert_opts)
           assert(dst_lnum > 0, "Destination could not be changed")
 
+          local dst_col = link_to_src_note:len() + 1
           vim.schedule(function() dst_note:open({ sync = true, line = dst_lnum, col = dst_col }) end)
         end,
       })
@@ -57,25 +57,30 @@ function M.new(...)
 end
 
 ---@param arg? my.obsidian.linker.ResolveNoteOpts
----@param callback fun(note?: obsidian.Note)
+---@param callback fun(note: obsidian.Note)
 function H.resolve_note(arg, callback)
+  local callback_unless_nil = function(opt_note)
+    if opt_note then callback(opt_note) end
+  end
+
   if type(arg) == "number" then
-    callback(require("obsidian.api").current_note(arg))
+    callback_unless_nil(require("obsidian.api").current_note(arg))
   elseif type(arg) == "string" then
     local func = assert(AUTO_RESOLVE[arg], string.format("unknown strategy: %q", arg))
-    func(callback)
+    func(callback_unless_nil)
   else ---@cast arg obsidian.Note|?
-    callback(arg)
+    callback_unless_nil(arg)
   end
 end
 
 function AUTO_RESOLVE.named(callback) require("obsidian.actions").new(nil, callback) end
 function AUTO_RESOLVE.unique(callback) callback(require("obsidian.actions").unique_note()) end
 
+--- Resolve a note by predefined strategy name (string), buffer number (integer), or explicit note.
 ---@alias my.obsidian.linker.ResolveNoteOpts
----|string to use a predefined resolution strategy (e.g. "named", "unique").
----|integer to use a specific note from a buffer
----|obsidian.Note to use a specific note
+---| string
+---| integer
+---| obsidian.Note
 
 ---@class my.obsidian.linker.LinkOpts
 ---@field link_fmt? string
