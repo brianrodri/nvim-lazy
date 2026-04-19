e934167ea246fbff67c48369c95d871755aa7996
Brian Rodriguez <brian@brianrodri.com>
2026-04-19 12:54:48 -0400

    Load Obsidian at startup

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -44,7 +44,7 @@ return {
     "obsidian-nvim/obsidian.nvim",
     commit = "d6c0e5bc30937df0657c9953d135d0ebb3af7e00",
     dependencies = { "nvim-lua/plenary.nvim", "folke/which-key.nvim", "folke/snacks.nvim" },
-    lazy = true,
+    lazy = false,
     enabled = VAULT:exists(),
     opts = {
       workspaces = { VAULT:get_workspace_spec() },
