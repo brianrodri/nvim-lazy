e603c598093f375717486e07add2f64cb0d58f3f
Brian Rodriguez <brian@brianrodri.com>
2026-04-19 20:10:00 -0400

    Reduce config footprint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -3,22 +3,12 @@
 return {
   {
     "stevearc/conform.nvim",
-    lazy = false,
     ---@type conform.setupOpts
-    opts = {
-      formatters = {
-        mdformat = { cwd = require("conform.util").root_file({ ".mdformat.toml" }) },
-      },
-    },
+    opts = { formatters = { mdformat = { cwd = require("conform.util").root_file({ ".mdformat.toml" }) } } },
   },
 
   {
     "mfussenegger/nvim-lint",
-    opts = {
-      lazy = false,
-      ---@module "lint"
-      ---@type table<string, lint.Linter|{}>
-      linters = { ["markdownlint-cli2"] = { append_fname = true, stdin = false } },
-    },
+    opts = { linters = { ["markdownlint-cli2"] = { append_fname = true, stdin = false } } },
   },
 }
